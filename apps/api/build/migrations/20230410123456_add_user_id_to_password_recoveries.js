e3149788ca93af84e0663002f9027166fec5e4ac
Jandy (Jitera) <developers@jitera.com>
2024-10-18 09:59:36 +0000

    fix: 3 issues from deployment report

--- apps/api/build/migrations/20230410123456_add_user_id_to_password_recoveries.js
+++ apps/api/build/migrations/20230410123456_add_user_id_to_password_recoveries.js
@@ -3,16 +3,9 @@ const { Sequelize } = require('sequelize');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('password_recoveries', 'user_id', {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      references: {
-      }
-        key: 'id'
-      },
-      onUpdate: 'CASCADE',
-      onDelete: 'SET NULL'
-    });
+    await queryInterface.sequelize.query(`
+      ALTER TABLE \`password_recoveries\` ADD COLUMN \`user_id\` INT NOT NULL, ADD CONSTRAINT \`fk_password_recoveries_user_id\` FOREIGN KEY (\`user_id\`) REFERENCES \`users\`(\`id\`);
+    `);
   },
 
   down: async (queryInterface, Sequelize) => {
